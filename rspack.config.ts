a9a622abb1d351ffc080a2849fd2d1918fcbee00
Kirill Ivanov <iwwwanow.work@yandex.ru>
2026-04-02 20:44:21 +0300

    fix(common): rspack configs

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,18 +1,33 @@
 import { defineConfig } from "@meteorjs/rspack";
 import { TsCheckerRspackPlugin } from "ts-checker-rspack-plugin";
 
-/**
- * Rspack configuration for Meteor projects.
- *
- * Provides typed flags on the `Meteor` object, such as:
- * - `Meteor.isClient` / `Meteor.isServer`
- * - `Meteor.isDevelopment` / `Meteor.isProduction`
- * - …and other flags available
- *
- * Use these flags to adjust your build settings based on environment.
- */
 export default defineConfig((/* Meteor */) => {
   return {
     plugins: [new TsCheckerRspackPlugin()],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: "builtin:swc-loader",
+          options: {
+            jsc: {
+              parser: {
+                syntax: "typescript",
+                decorators: true,
+                tsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: "automatic",
+                },
+                decoratorMetadata: true,
+                legacyDecorator: true,
+              },
+            },
+          },
+        },
+      ],
+    },
   };
 });
